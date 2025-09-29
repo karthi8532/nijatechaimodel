15392d48db9b86dc20adbd36b46e77e84461e05c
NijaTech <nijatech@NijaTechs-MacBook-Air.local>
2025-09-29 13:23:53 +0530

    add outlook

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2472489293"
+    serviceWorkerVersion: "2114289983"
   }
 });
